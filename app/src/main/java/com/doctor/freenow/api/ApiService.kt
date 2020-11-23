a5302a94e6547de66b0e9cc1850ab19622b7baf9
Sachin <sachinkmr375@gmail.com>
2020-11-23 22:09:16 +0530

    added interface for retrofit

--- app/src/main/java/com/doctor/freenow/api/ApiService.kt
+++ app/src/main/java/com/doctor/freenow/api/ApiService.kt
@@ -1,5 +1,14 @@
 package com.doctor.freenow.api
 
+import com.doctor.freenow.model.Poi
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
 
+    @GET("")
+    suspend fun getVehicleList(@Query("p1Lat") p1Lat: String,
+    @Query("p1Lon") p1Lon: String,
+    @Query("p2Lat") p2Lat: String,
+    @Query("p2Lon") p2Lon: String): List<Poi>
 }
