64720cf33e798c9827e6204dafe82a48de5665d0
Sachin <sachinkmr375@gmail.com>
2020-11-24 15:16:37 +0530

    added viewmodel and string utils

---
+++ app/src/main/java/com/doctor/freenow/utils/StringUtils.kt
@@ -0,0 +1,9 @@
+package com.doctor.freenow.utils
+
+object StringUtils {
+    const val ERROR_MSG = "Something Went Wrong"
+    const val p1Lat = "53.694865"
+    const val p1Lon = "9.757589"
+    const val p2Lat = "53.394655"
+    const val p2Lon = "10.099891"
+}
---
+++ app/src/main/java/com/doctor/freenow/viewmodel/VehicleListViewModel.kt
@@ -0,0 +1,33 @@
+package com.doctor.freenow.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.doctor.freenow.api.ApiHelper
+import com.doctor.freenow.model.Poi
+import com.doctor.freenow.utils.Resource
+import com.doctor.freenow.utils.StringUtils
+import kotlinx.coroutines.launch
+
+class VehicleListViewModel(private val apiHelper: ApiHelper) : ViewModel() {
+
+    private val vehicles = MutableLiveData<Resource<List<Poi>>>()
+
+    fun fetchVehicles() {
+        viewModelScope.launch {
+            vehicles.postValue(Resource.loading(null))
+            try {
+                val usersFromApi = apiHelper.getVehicleList(StringUtils.p1Lat, StringUtils.p1Lon, StringUtils.p2Lat, StringUtils.p2Lon)
+                vehicles.postValue(Resource.success(usersFromApi))
+            } catch (e: Exception) {
+                vehicles.postValue(Resource.error(StringUtils.ERROR_MSG, null))
+            }
+        }
+    }
+
+    fun getVehicles(): LiveData<Resource<List<Poi>>> {
+        return vehicles
+    }
+
+}
